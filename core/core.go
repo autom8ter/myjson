6cb36c0ad04a5c4a6ff1d13add28ccf451dab9e7
autom8ter <coleman.word@autom8ter.com>
2022-10-26 18:48:16 -0600

    more comments

--- core/core.go
+++ core/core.go
@@ -6,19 +6,31 @@ import (
 	"io"
 )
 
+// CoreAPI is the core api powering database functionality
 type CoreAPI interface {
+	//  Persist persists changes to a collection
 	Persist(ctx context.Context, collection *Collection, change StateChange) error
+	// Aggregate aggregates documents in the database
 	Aggregate(ctx context.Context, collection *Collection, query AggregateQuery) (Page, error)
+	// Search executes a full text search
 	Search(ctx context.Context, collection *Collection, query SearchQuery) (Page, error)
+	// Query queries for documents
 	Query(ctx context.Context, collection *Collection, query Query) (Page, error)
+	// Get gets a single document
 	Get(ctx context.Context, collection *Collection, id string) (*Document, error)
+	// Get gets a list of documents
 	GetAll(ctx context.Context, collection *Collection, ids []string) ([]*Document, error)
+	// ChangeStream streams state changes to the given function
 	ChangeStream(ctx context.Context, collection *Collection, fn ChangeStreamHandler) error
+	// Backup backs up the database to the given writer
 	Backup(ctx context.Context, w io.Writer, since uint64) error
+	// Restore restores the database from the given reader
 	Restore(ctx context.Context, r io.Reader) error
+	// Close closes the database
 	Close(ctx context.Context) error
 }
 
+// Core implements the CoreAPI interface
 type Core struct {
 	persist      PersistFunc
 	aggregate    AggregateFunc
@@ -32,56 +44,67 @@ type Core struct {
 	restore      RestoreFunc
 }
 
+// WithPersist sets the persist func
 func (c Core) WithPersist(fn PersistFunc) Core {
 	c.persist = fn
 	return c
 }
 
+// WithAggregate sets the aggregate func
 func (c Core) WithAggregate(fn AggregateFunc) Core {
 	c.aggregate = fn
 	return c
 }
 
+// WithSearch sets the search func
 func (c Core) WithSearch(fn SearchFunc) Core {
 	c.search = fn
 	return c
 }
 
+// WithQuery sets the query func
 func (c Core) WithQuery(fn QueryFunc) Core {
 	c.query = fn
 	return c
 }
 
+// WithGet sets the get func
 func (c Core) WithGet(fn GetFunc) Core {
 	c.get = fn
 	return c
 }
 
+// WithGetAll sets the get all func
 func (c Core) WithGetAll(fn GetAllFunc) Core {
 	c.getAll = fn
 	return c
 }
 
+// WithChangeStream sets the change stream func
 func (c Core) WithChangeStream(fn ChangeStreamFunc) Core {
 	c.changeStream = fn
 	return c
 }
 
+// WithClose sets the close func
 func (c Core) WithClose(fn CloseFunc) Core {
 	c.close = fn
 	return c
 }
 
+// WithBackup sets the backupfunc
 func (c Core) WithBackup(fn BackupFunc) Core {
 	c.backup = fn
 	return c
 }
 
+// WithRestore sets the restorefunc
 func (c Core) WithRestore(fn RestoreFunc) Core {
 	c.restore = fn
 	return c
 }
 
+// Backup backs up the database
 func (c Core) Backup(ctx context.Context, w io.Writer, since uint64) error {
 	if c.backup == nil {
 		return fmt.Errorf("unimplemented")
@@ -89,6 +112,7 @@ func (c Core) Backup(ctx context.Context, w io.Writer, since uint64) error {
 	return c.backup(ctx, w, since)
 }
 
+// Restore restores the database from the reader
 func (c Core) Restore(ctx context.Context, r io.Reader) error {
 	if c.restore == nil {
 		return fmt.Errorf("unimplemented")
@@ -96,6 +120,7 @@ func (c Core) Restore(ctx context.Context, r io.Reader) error {
 	return c.restore(ctx, r)
 }
 
+// Close closes the database
 func (c Core) Close(ctx context.Context) error {
 	if c.close == nil {
 		return fmt.Errorf("unimplemented")
@@ -103,6 +128,7 @@ func (c Core) Close(ctx context.Context) error {
 	return c.close(ctx)
 }
 
+// Persist persists a state change. Persist is the only way to execute a mutation
 func (c Core) Persist(ctx context.Context, collection *Collection, change StateChange) error {
 	if c.persist == nil {
 		return fmt.Errorf("unimplemented")
@@ -110,6 +136,7 @@ func (c Core) Persist(ctx context.Context, collection *Collection, change StateC
 	return c.persist(ctx, collection, change)
 }
 
+// Aggregate aggregates documents- the return type is not compatible with the collection's schema
 func (c Core) Aggregate(ctx context.Context, collection *Collection, query AggregateQuery) (Page, error) {
 	if c.aggregate == nil {
 		return Page{}, fmt.Errorf("unimplemented")
@@ -117,6 +144,7 @@ func (c Core) Aggregate(ctx context.Context, collection *Collection, query Aggre
 	return c.aggregate(ctx, collection, query)
 }
 
+// Search executes a full text search
 func (c Core) Search(ctx context.Context, collection *Collection, query SearchQuery) (Page, error) {
 	if c.search == nil {
 		return Page{}, fmt.Errorf("unimplemented")
@@ -124,6 +152,7 @@ func (c Core) Search(ctx context.Context, collection *Collection, query SearchQu
 	return c.search(ctx, collection, query)
 }
 
+// Query queries a set of documents
 func (c Core) Query(ctx context.Context, collection *Collection, query Query) (Page, error) {
 	if c.query == nil {
 		return Page{}, fmt.Errorf("unimplemented")
@@ -131,6 +160,7 @@ func (c Core) Query(ctx context.Context, collection *Collection, query Query) (P
 	return c.query(ctx, collection, query)
 }
 
+// Get gets a document by id
 func (c Core) Get(ctx context.Context, collection *Collection, id string) (*Document, error) {
 	if c.get == nil {
 		return nil, fmt.Errorf("unimplemented")
@@ -138,6 +168,7 @@ func (c Core) Get(ctx context.Context, collection *Collection, id string) (*Docu
 	return c.get(ctx, collection, id)
 }
 
+// GetAll gets all documents by id
 func (c Core) GetAll(ctx context.Context, collection *Collection, ids []string) ([]*Document, error) {
 	if c.getAll == nil {
 		return nil, fmt.Errorf("unimplemented")
@@ -145,6 +176,7 @@ func (c Core) GetAll(ctx context.Context, collection *Collection, ids []string) 
 	return c.getAll(ctx, collection, ids)
 }
 
+// ChangeStream streams state changes to the provided function
 func (c Core) ChangeStream(ctx context.Context, collection *Collection, fn ChangeStreamHandler) error {
 	if c.changeStream == nil {
 		return fmt.Errorf("unimplemented")
@@ -152,6 +184,7 @@ func (c Core) ChangeStream(ctx context.Context, collection *Collection, fn Chang
 	return c.changeStream(ctx, collection, fn)
 }
 
+// Middleware is a set of wrapper functions that alter core functionality
 type Middleware struct {
 	Persist      []PersistWare
 	Aggregate    []AggregateWare
@@ -162,6 +195,7 @@ type Middleware struct {
 	ChangeStream []ChangeStreamWare
 }
 
+// Apply applies the middleware and returns a new Core instance
 func (c Core) Apply(m Middleware) Core {
 	core := Core{
 		persist:      c.persist,
