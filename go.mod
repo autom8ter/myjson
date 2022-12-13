ddc864160f9b4a6bbf72f38c61e3f1b725ae9704
autom8ter <coleman.word@autom8ter.com>
2022-12-12 21:55:46 -0700

    http endpoints

--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgraph-io/ristretto v0.1.0 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
--- http.go
+++ http.go
@@ -1,44 +1,45 @@
 package gokvkit
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"net/http"
-	"strings"
 
-	"github.com/autom8ter/gokvkit/kv"
 	"github.com/autom8ter/gokvkit/model"
+	"github.com/go-chi/chi/v5"
 	"github.com/palantir/stacktrace"
 )
 
 // Handler is an http handler that serves database commands and queries
 func (db *DB) Handler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasSuffix(r.URL.Path, "/spec") {
-			specHandler(db)
-			return
-		}
-		path := strings.Split(r.URL.Path, "/")
-		op := path[len(path)-1]
-		collection := path[len(path)-2]
-		handler := db.router.Get(collection, op, r.Method)
-		if handler == nil {
-			httpError(w, stacktrace.NewErrorWithCode(http.StatusBadRequest, "%s %s | zero operations for request", r.Method, r.RequestURI))
-			return
-		}
-		md, _ := model.GetMetadata(r.Context())
-		for k, v := range r.URL.Query() {
-			md.Set(k, v[0])
-		}
-		for k, v := range r.Header {
-			md.Set(k, v[0])
-		}
-		handler.ServeHTTP(w, r.WithContext(md.ToContext(r.Context())))
-	})
+	mux := chi.NewRouter()
+	mux.Get("/spec", specHandler(db))
+
+	mux.Post("/collections/{collection}", createDocHandler(db))
+
+	mux.Put("/collections/{collection}/{docID}", setDocHandler(db))
+	mux.Patch("/collections/{collection}/{docID}", patchDocHandler(db))
+	mux.Delete("/collections/{collection}/{docID}", deleteDocHandler(db))
+	mux.Get("/collections/{collection}/{docID}", getDocHandler(db))
+
+	mux.Post("/collections/{collection}/_/query", queryHandler(db))
+	mux.Post("/collections/{collection}/_/batch", batchSetHandler(db))
+
+	mux.Get("/schema", getSchemasHandler(db))
+	mux.Get("/schema/{collection}", getSchemaHandler(db))
+	mux.Put("/schema/{collection}", putSchemaHandler(db))
+
+	return mux
 }
 
-func queryHandler(collection string, db *DB) http.Handler {
+func queryHandler(db *DB) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		collection := chi.URLParam(r, "collection")
+		if !db.hasCollection(collection) {
+			httpError(w, stacktrace.NewErrorWithCode(http.StatusBadRequest, "collection does not exist"))
+			return
+		}
 		var q model.Query
 		if err := json.NewDecoder(r.Body).Decode(&q); err != nil {
 			httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to decode query"))
@@ -54,60 +55,211 @@ func queryHandler(collection string, db *DB) http.Handler {
 	})
 }
 
-func commandHandler(collection string, db *DB) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var commands []*model.Command
-		if err := json.NewDecoder(r.Body).Decode(&commands); err != nil {
-			httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to decode command"))
+func createDocHandler(db *DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		collection := chi.URLParam(r, "collection")
+		if !db.hasCollection(collection) {
+			httpError(w, stacktrace.NewErrorWithCode(http.StatusBadRequest, "collection does not exist"))
 			return
 		}
-		err := db.kv.Tx(true, func(tx kv.Tx) error {
-			return db.persistStateChange(r.Context(), tx, commands)
-		})
+		var doc = model.NewDocument()
+		if err := json.NewDecoder(r.Body).Decode(doc); err != nil {
+			httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to decode query"))
+			return
+		}
+		if err := db.Tx(r.Context(), func(ctx context.Context, tx Tx) error {
+			id, err := tx.Create(ctx, collection, doc)
+			if err != nil {
+				return stacktrace.Propagate(err, "")
+			}
+			if err := db.setPrimaryKey(collection, doc, id); err != nil {
+				return stacktrace.Propagate(err, "")
+			}
+			return nil
+		}); err != nil {
+			httpError(w, err)
+			return
+		}
+		json.NewEncoder(w).Encode(doc)
+	}
+}
+
+func patchDocHandler(db *DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		collection := chi.URLParam(r, "collection")
+		if !db.hasCollection(collection) {
+			httpError(w, stacktrace.NewErrorWithCode(http.StatusBadRequest, "collection does not exist"))
+			return
+		}
+		docID := chi.URLParam(r, "docID")
+		var update = map[string]any{}
+		if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
+			httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to decode edit"))
+			return
+		}
+		if err := db.Tx(r.Context(), func(ctx context.Context, tx Tx) error {
+			err := tx.Update(ctx, collection, docID, update)
+			if err != nil {
+				return stacktrace.Propagate(err, "")
+			}
+			return nil
+		}); err != nil {
+			httpError(w, err)
+			return
+		}
+		doc, err := db.Get(r.Context(), collection, docID)
 		if err != nil {
+			httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to edit document"))
+			return
+		}
+		json.NewEncoder(w).Encode(doc)
+	}
+}
+
+func setDocHandler(db *DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		collection := chi.URLParam(r, "collection")
+		if !db.hasCollection(collection) {
+			httpError(w, stacktrace.NewErrorWithCode(http.StatusBadRequest, "collection does not exist"))
+			return
+		}
+		docID := chi.URLParam(r, "docID")
+		var doc = model.NewDocument()
+		if err := json.NewDecoder(r.Body).Decode(&doc); err != nil {
+			httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to decode update"))
+			return
+		}
+		if err := db.Tx(r.Context(), func(ctx context.Context, tx Tx) error {
+			err := tx.Set(ctx, collection, doc)
+			if err != nil {
+				return stacktrace.Propagate(err, "")
+			}
+			return nil
+		}); err != nil {
+			httpError(w, err)
+			return
+		}
+		doc, err := db.Get(r.Context(), collection, docID)
+		if err != nil {
+			httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to edit document"))
+			return
+		}
+		json.NewEncoder(w).Encode(doc)
+	}
+}
+
+func getDocHandler(db *DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		collection := chi.URLParam(r, "collection")
+		if !db.hasCollection(collection) {
+			httpError(w, stacktrace.NewErrorWithCode(http.StatusBadRequest, "collection does not exist"))
+			return
+		}
+		docID := chi.URLParam(r, "docID")
+		doc, err := db.Get(r.Context(), collection, docID)
+		if err != nil {
+			httpError(w, stacktrace.PropagateWithCode(err, http.StatusNotFound, "failed to get document"))
+			return
+		}
+		json.NewEncoder(w).Encode(doc)
+	}
+}
+
+func deleteDocHandler(db *DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		collection := chi.URLParam(r, "collection")
+		if !db.hasCollection(collection) {
+			httpError(w, stacktrace.NewErrorWithCode(http.StatusBadRequest, "collection does not exist"))
+			return
+		}
+		docID := chi.URLParam(r, "docID")
+		if err := db.Tx(r.Context(), func(ctx context.Context, tx Tx) error {
+			err := tx.Delete(ctx, collection, docID)
+			if err != nil {
+				return stacktrace.Propagate(err, "")
+			}
+			return nil
+		}); err != nil {
 			httpError(w, err)
 			return
 		}
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(&commands)
+	}
+}
+
+func batchSetHandler(db *DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		collection := chi.URLParam(r, "collection")
+		if !db.hasCollection(collection) {
+			httpError(w, stacktrace.NewErrorWithCode(http.StatusBadRequest, "collection does not exist"))
+			return
+		}
+		var docs []*model.Document
+		if err := json.NewDecoder(r.Body).Decode(&docs); err != nil {
+			httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to decode query"))
+			return
+		}
+		if err := db.Tx(r.Context(), func(ctx context.Context, tx Tx) error {
+			for _, d := range docs {
+				if err := tx.Set(ctx, collection, d); err != nil {
+					return stacktrace.Propagate(err, "")
+				}
+			}
+			return nil
+		}); err != nil {
+			httpError(w, err)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
 	})
 }
 
-func schemaHandler(collection string, db *DB) http.HandlerFunc {
+func getSchemasHandler(db *DB) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			if !db.hasCollection(collection) {
-				httpError(w, stacktrace.NewErrorWithCode(http.StatusBadRequest, "collection does not exist"))
-				return
-			}
-			bits, _ := db.getCollectionSchema(collection)
-			w.WriteHeader(http.StatusOK)
-			w.Write(bits)
-		case http.MethodPut:
-			bits, err := io.ReadAll(r.Body)
-			if err != nil {
-				httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to read request body"))
-				return
-			}
-			if err := db.ConfigureCollection(r.Context(), bits); err != nil {
-				httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to configure collection"))
-				return
-			}
-			w.WriteHeader(http.StatusOK)
+		schemas, _ := db.getPersistedCollections()
+		var resp = map[string]string{}
+		schemas.RangeR(func(key string, c *collectionSchema) bool {
+			resp[c.collection] = c.raw.Raw
+			return true
+		})
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(&resp)
+	})
+}
+
+func getSchemaHandler(db *DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		collection := chi.URLParam(r, "collection")
+		if !db.hasCollection(collection) {
+			httpError(w, stacktrace.NewErrorWithCode(http.StatusBadRequest, "collection does not exist"))
+			return
 		}
+		bits, _ := db.getCollectionSchema(collection)
+		w.WriteHeader(http.StatusOK)
+		w.Write(bits)
+	})
+}
+
+func putSchemaHandler(db *DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bits, err := io.ReadAll(r.Body)
+		if err != nil {
+			httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to read request body"))
+			return
+		}
+		if err := db.ConfigureCollection(r.Context(), bits); err != nil {
+			httpError(w, stacktrace.PropagateWithCode(err, http.StatusBadRequest, "failed to configure collection"))
+			return
+		}
+		w.WriteHeader(http.StatusOK)
 	})
 }
 
 func specHandler(db *DB) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			bits, _ := getOpenAPISpec(db.collections, &db.openAPIParams)
-			w.WriteHeader(http.StatusOK)
-			w.Write(bits)
-
-		}
+		bits, _ := getOpenAPISpec(db.collections, &db.openAPIParams)
+		w.WriteHeader(http.StatusOK)
+		w.Write(bits)
 	})
 }
 
