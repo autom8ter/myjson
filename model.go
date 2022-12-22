6349f976b0a23ece5cbb14d1a45bbb500dfe19a2
autom8ter <coleman.word@autom8ter.com>
2022-12-21 17:03:47 -0700

    update

--- model.go
+++ model.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 	"time"
 
+	_ "embed"
+
 	"github.com/autom8ter/gokvkit/errors"
 	"github.com/autom8ter/gokvkit/util"
 	"github.com/samber/lo"
@@ -319,4 +321,37 @@ type OnRollback struct {
 	Before bool
 	// Func is the function to execute
 	Func func(ctx context.Context, tx Tx) `validate:"required"`
+}
+
+//go:embed cdc.yaml
+var cdcSchema string
+
+const cdcCollectionName = "cdc"
+
+type JSONOp string
+
+const (
+	JSONOpRemove  JSONOp = "remove"
+	JSONOpAdd     JSONOp = "add"
+	JSONOpReplace JSONOp = "replace"
+)
+
+// JSONFieldOp
+type JSONFieldOp struct {
+	Path        string `json:"path"`
+	Op          JSONOp `json:"op"`
+	Value       any    `json:"value,omitempty"`
+	BeforeValue any    `json:"beforeValue,omitempty"`
+}
+
+// CDC is a change data capture object used for tracking changes to documents over time
+type CDC struct {
+	ID         string        `json:"_id" validate:"required"`
+	Collection string        `json:"collection" validate:"required"`
+	Action     Action        `json:"action" validate:"required,oneof='create' 'update' 'delete' 'set'"`
+	DocumentID string        `json:"documentID" validate:"required"`
+	Document   *Document     `json:"document" validate:"required"`
+	Diff       []JSONFieldOp `json:"diff,omitempty"`
+	Timestamp  time.Time     `json:"timestamp" validate:"required"`
+	Metadata   *Metadata     `json:"metadata" validate:"required"`
 }
