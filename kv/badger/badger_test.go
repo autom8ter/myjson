cf9d415411230b88f3f6cd8e61be8bb01904ec87
autom8ter <coleman.word@autom8ter.com>
2022-12-29 18:32:44 -0700

    more badger tests

--- kv/badger/badger_test.go
+++ kv/badger/badger_test.go
@@ -107,6 +107,9 @@ func Test(t *testing.T) {
 			gotLock, err := lock.TryLock()
 			assert.NoError(t, err)
 			assert.True(t, gotLock)
+			is, err := lock.IsLocked()
+			assert.NoError(t, err)
+			assert.True(t, is)
 		}
 		{
 			gotLock, err := lock.TryLock()
@@ -143,6 +146,58 @@ func Test(t *testing.T) {
 			}
 			return nil
 		}))
+	})
+	t.Run("new tx", func(t *testing.T) {
+		tx := db.NewTx(true)
+		defer func() {
+			assert.NoError(t, tx.Commit())
+		}()
+		for k, v := range data {
+			assert.Nil(t, tx.Set([]byte(k), []byte(v), 3*time.Second))
+		}
+		for k, _ := range data {
+			_, err := tx.Get([]byte(k))
+			assert.NoError(t, err)
+		}
+		time.Sleep(3 * time.Second)
+		for k, _ := range data {
+			bytes, _ := tx.Get([]byte(k))
+			assert.Nil(t, bytes)
+		}
+	})
+	t.Run("new tx w/ rollback", func(t *testing.T) {
+		tx := db.NewTx(true)
+
+		for k, v := range data {
+			assert.Nil(t, tx.Set([]byte(k), []byte(v), 0))
+		}
+		tx.Rollback()
+		for k, _ := range data {
+			val, _ := tx.Get([]byte(k))
+			assert.Empty(t, val)
+		}
+	})
+	t.Run("drop prefix", func(t *testing.T) {
+		{
+			tx := db.NewTx(true)
+			for k, v := range data {
+				assert.Nil(t, tx.Set([]byte(fmt.Sprintf("testing.%s", k)), []byte(v), 0))
+			}
+			assert.NoError(t, tx.Commit())
+		}
+		assert.NoError(t, db.DropPrefix([]byte("testing.")))
+		count := 0
+		assert.NoError(t, db.Tx(false, func(tx kv.Tx) error {
+			iter := tx.NewIterator(kv.IterOpts{Prefix: []byte("testing.")})
+			defer iter.Close()
+			for iter.Valid() {
+				_ = iter.Item()
+				count++
+				iter.Next()
+			}
+			return nil
+		}))
+		assert.Equal(t, 0, count)
 	})
 
 }
